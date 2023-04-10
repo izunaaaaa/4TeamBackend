c83ac660e3cf02829c0a2df02a33609f81ee3d7a
KimDuHong <art970@naver.com>
2023-04-10 17:58:16 +0900

    add cors

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
