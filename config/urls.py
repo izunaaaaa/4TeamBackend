20dc83c28299314c6cf2e7325376b9e620e09186
goeasyonng <meoa1026@gmail.com>
2023-04-10 15:16:16 +0900

    api title

--- config/urls.py
+++ config/urls.py
@@ -23,9 +23,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="BangSam BackEnd API",
+        title="CurB backend API",
         default_version="v1",
-        description="https://github.com/izunaaaaa/BangSam",
+        description="https://github.com/izunaaaaa/CurB_Backend",
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
