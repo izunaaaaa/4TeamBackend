35c205b1a4df7b1ec28553ff76747d7b9ea8ef5b
song <song@songui-MacBookAir.local>
2023-04-12 16:49:12 +0900

    category view

--- categories/models.py
+++ categories/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from common.models import CommonModel
-
+from groups.models import Group
 
 class Category(CommonModel):
     name = models.CharField(max_length=100)
@@ -8,18 +8,30 @@ class Category(CommonModel):
         "groups.Group",
         on_delete=models.CASCADE,
     )
-    unique_together = ("name", "group")
 
     def __str__(self) -> str:
         return f"{self.name}"
 
     class Meta:
         verbose_name_plural = "categories"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "group"], 
+                name="unique_category_name_group",
+            )
+        ]
 
     # def add_default_data():
     #     Category.objects.get_or_create(name='전체글')
 
-    def save(self):
-        if not self.name:
-            self.name = "전체글"
-        super().save()
+    # def save(self):
+    #     if not self.name:
+    #         self.name = "전체글"
+    #     super().save()
+
+    class initial_categroy(Group):
+        def save(self, *args, **kwargs):
+            is_new_group = not bool(self.pk) # check if the group is being created
+            super().save(*args, **kwargs)
+            if is_new_group:
+                Category.objects.create(name='전체글', group=self)
--- categories/views.py
+++ categories/views.py
@@ -3,6 +3,7 @@ from drf_yasg import openapi
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound, PermissionDenied, ParseError
+from rest_framework.permissions import IsAuthenticated
 from .models import Category
 from . import serializers
 from django.shortcuts import get_object_or_404
@@ -10,16 +11,11 @@ from groups.models import Group
 
 
 class Categories(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
     @swagger_auto_schema(
-        operation_summary="카테고리 조회 api",
-        manual_parameters=[
-            openapi.Parameter(
-                "group",
-                openapi.IN_QUERY,
-                description="그룹 데이터 \n - group의 name로 작성",
-                type=openapi.TYPE_STRING,
-            ),
-        ],
+        operation_summary="전체 카테고리 조회 api",
         responses={
             200: openapi.Response(
                 description="Successful Response",
@@ -28,7 +24,28 @@ class Categories(APIView):
         },
     )
     def get(self, request):
-        group = get_object_or_404(Group, name=request.GET.get("group"))
+        category = Category.objects.all()
+        serializer = serializers.CategorySerializer(
+            category,
+            many=True,
+        )
+        return Response(serializer.data)
+    
+class GroupCategories(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    @swagger_auto_schema(
+        operation_summary="그룹 카테고리 조회 api",
+        responses={
+            200: openapi.Response(
+                description="Successful Response",
+                schema=serializers.CategorySerializer(),
+            )
+        },
+    )
+    def get(self, request, group):
+        group = get_object_or_404(Group, name=group)
         category = Category.objects.filter(group=group)
         serializer = serializers.CategorySerializer(
             category,
@@ -37,17 +54,9 @@ class Categories(APIView):
         return Response(serializer.data)
 
     @swagger_auto_schema(
-        operation_summary="카테고리 생성 api",
+        operation_summary="그룹 카테고리 생성 api",
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
-            manual_parameters=[
-            openapi.Parameter(
-                "group",
-                openapi.IN_QUERY,
-                description="그룹 데이터 \n - group의 name로 작성",
-                type=openapi.TYPE_STRING,
-            ),
-        ],
             required=["name"],
             properties={
                 "name": openapi.Schema(type=openapi.TYPE_STRING, description="카테고리명"),
@@ -59,22 +68,46 @@ class Categories(APIView):
             401: openapi.Response(description="The user is not authenticated"),
         },
     )
-    def post(self, request):
+    def post(self, request, group):
         serializer = serializers.CategorySerializer(data=request.data)
         
-        #만약 request.user이 is_coach가 false인경우
-        if request.user.is_coach == False:
-            raise PermissionDenied
-        
+        # if request.user.is_coach == False:
+            # raise PermissionDenied
         if serializer.is_valid():
-            group = request.GET.get("group")
-            group = get_object_or_404(Group, name=group) 
-            name = request.data.get("name")
-            if Group.objects.filter(name=name).exists():
-                raise ParseError("that group already exists")
-            category = serializer.save(group=group, name=name,)
+            category = serializer.save(group=group)
             serializer = serializers.CategorySerializer(category)
-            return Response(serializer.data)
+            return Response(serializer.data, status=201)
         else:
             return Response(serializer.errors, status=400)
     
+class GroupCategoryDetail(APIView):
+    @swagger_auto_schema(
+        operation_summary="그룹 카테고리 조회 api",
+        responses={
+            200: openapi.Response(
+                description="Successful Response",
+                schema=serializers.CategorySerializer(),
+            )
+        },
+    )
+    def get(self, request, pk, group):
+        category = get_object_or_404(Category, pk=pk, group__name=group)
+        serializer = serializers.CategorySerializer(category)
+        return Response(serializer.data)
+    
+    @swagger_auto_schema(
+        operation_summary="그룹 카테고리 삭제 api",
+        responses={
+            200: openapi.Response(
+                description="Successful response",
+                schema=serializers.CategorySerializer(many=True),
+            )
+        },
+    )
+    def delete(self, request, pk,group):
+        if request.user.is_coach == False:
+            raise PermissionDenied
+        group = get_object_or_404(Group, name=group)
+        category = Category.objects.filter(group=group,pk=pk)
+        category.delete()
+        return Response({"result":"delete success"})
