e8edfdb5ce2a856b098568e03b0eafecfe058880
song <song@songui-MacBookAir.local>
2023-04-13 14:31:17 +0900

    feed recoment show

--- comments/serializers.py
+++ comments/serializers.py
@@ -6,7 +6,7 @@ from recomments.serializers import RecommentSerializer
 
 class CommentSerializer(ModelSerializer):
     user = TinyUserSerializer(read_only=True)
-    recomment = RecommentSerializer(read_only=True)
+    recomment = RecommentSerializer(read_only=True, many=True)
 
     class Meta:
         model = Comment
