c477e6e8480530d2987b0c505d52c12b9cb30189
song <song@songui-MacBookAir.local>
2023-04-13 16:40:05 +0900

    recomment user show

--- recomments/serializers.py
+++ recomments/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Recomment
-
+from users.serializers import TinyUserSerializer
 
 class RecommentSerializer(ModelSerializer):
+    user = TinyUserSerializer(read_only=True)
+    
     class Meta:
         model = Recomment
-        exclude = ("updated_at",)
+        fields = (        
+        "user",
+                  "created_at",
+        "description",
+        "comment",)
