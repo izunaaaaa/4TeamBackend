aac63087f1192e49d0420c303475a3e5c0c0e377
KimDuHong <art970@naver.com>
2023-04-18 20:46:12 +0900

    edit feed api

--- feeds/urls.py
+++ feeds/urls.py
@@ -3,13 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.Feeds.as_view()),  # 모든 게시글 보여주기, 게시글 업로드
-    path("<int:pk>", views.FeedDetail.as_view()),  # 게시글 수정 / 삭제
-    path("group/", views.GroupFeeds.as_view()),
-    path("group/<int:pk>", views.GroupFeeds.as_view()),
-    path(
-        "group/category/",
-        views.GroupFeedCategory.as_view(),
-    ),
-    # path("group/category/detail", views.GroupFeedDetail.as_view()),
-    path("toplike", views.TopLikeView.as_view()),
+    path("<int:pk>/", views.FeedDetail.as_view()),  # 게시글 디테일 / 게시글 수정 / 삭제
+    path("group/category/", views.GroupFeedCategory.as_view()),
+    path("toplike/", views.TopLikeView.as_view()),
+    # path("group/", views.GroupFeeds.as_view()),
 ]
