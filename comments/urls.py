03d637d688dc7d6764c0a91e5b1f1a11935818a0
KimDuHong <art970@naver.com>
2023-04-20 21:52:32 +0900

    add comment delete

--- comments/urls.py
+++ comments/urls.py
@@ -2,8 +2,6 @@ from rest_framework.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.Comments.as_view()),
-    # path("<int:pk>", views.CommentDetail.as_view()),
-    # path("<int:pk>/recomments", views.Recomments.as_view()),
-    # path("toplike", views.TopLikeView.as_view()),
+    path("<int:pk>", views.CommentDetail.as_view()),
+    path("<int:comment_pk>/recomments/<int:recomment_pk>", views.Recomments.as_view()),
 ]
