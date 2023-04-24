411ce631092b0fcc8883467c79fea91ee01c32cd
goeasyonng <song@songui-MacBookAir.local>
2023-04-24 21:40:43 +0900

    test

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestTask(APITestCase):
+    URL = "/api/v1/categories/"
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.TITLE = "Categories Test"
+        cls.CONTENT = "Categories Test"
+
+    def test_view(self):
+        response = self.client.get(self.URL)
+        self.assertEqual(response.status_code, 403, "비 로그인 조회")
