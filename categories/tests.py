82f862bac17d42c23dabfc848d053439d2fda328
goeasyonng <song@songui-MacBookAir.local>
2023-05-01 17:41:24 +0900

    category testcode

--- categories/tests.py
+++ categories/tests.py
@@ -5,88 +5,91 @@ from . import serializers
 from users.models import User
 
 
-# 카테고리 조회 테스트
-class CategoriesGet(APITestCase):
+# 전체 카테고리 조회 테스트
+# class CategoriesGet(APITestCase):
+#     URL = "/api/v1/categories/"
+#     NAME = "Category get test"
+
+#     def setUp(self):
+#         self.GROUP = Group.objects.create(name="oz")
+#         Category.objects.create(
+#             name=self.NAME,
+#             group=self.GROUP,
+#         )
+
+#     def test_all_category(self):
+#         response = self.client.get(self.URL)
+#         data = response.json()
+#         self.assertEqual(response.status_code, 200, "status isn't 200")
+#         self.assertEqual(len(response.data), 4)
+
+
+# 그룹 카테고리 조회 테스트
+class GroupCategoriesGet(APITestCase):
     URL = "/api/v1/categories/"
-    NAME = "Category Test"
+    NAME = "Group category get test"
 
     def setUp(self):
-        self.GROUP = Group.objects.create(name="oz")
-        Category.objects.create(
+        self.group = Group.objects.create(name="oz")
+        self.user = User.objects.create(is_coach=True)
+        self.category = Category.objects.create(
             name=self.NAME,
-            group=self.GROUP,
+            group=self.group,
         )
 
-    def test_all_category(self):
-        response = self.client.get(self.URL)
-        data = response.json()
+    def get_all_category(self):
+        self.client.force_login(self.user)
+        response = self.client.get(f"{self.URL}{self.group.pk}")
         self.assertEqual(response.status_code, 200, "status isn't 200")
         self.assertEqual(len(response.data), 4)
 
 
-# 그룹 카테고리 조회 테스트
-
-
 # 그룹 카테고리 생성 테스트
-class CategoriesPost(APITestCase):
+class GroupCategoriesPost(APITestCase):
     URL = "/api/v1/categories/"
-    NAME = "Category Test"
+    NAME = "Group category get test"
 
     def setUp(self):
-        self.GROUP = Group.objects.create(name="oz")
+        self.group = Group.objects.create(name="oz")
         self.user = User.objects.create(is_coach=True)
         self.category = Category.objects.create(
             name=self.NAME,
-            group=self.GROUP,
+            group=self.group,
         )
 
+    # 유효한 데이터로 POST 요청 보내기
     def test_category_post_with_valid_data(self):
-        # 유효한 데이터로 POST 요청 보내기
         data = {"name": "study"}
         self.client.force_login(self.user)
-        response = self.client.post(f"{self.URL}{self.GROUP.pk}", data, format="json")
-        # 응답 코드가 201인지 확인
+        response = self.client.post(f"{self.URL}{self.group.pk}", data, format="json")
         self.assertEqual(response.status_code, 201)
 
-        # 응답 데이터가 올바른지 확인
-        # category = self.GROUP.categories.first()
-        # serializer = serializers.CategorySerializer(category)
-        # self.assertEqual(response.data, data)
-
+    # 유효하지 않은 데이터로 POST 요청 보내기
     def test_category_post_with_invalid_data(self):
-        # 유효하지 않은 데이터로 POST 요청 보내기
         data = {"name": ""}
         self.client.force_login(self.user)
-        response = self.client.post(f"{self.URL}{self.GROUP.pk}", data, format="json")
-        # 응답 코드가 400인지 확인
+        response = self.client.post(f"{self.URL}{self.group.pk}", data, format="json")
         self.assertEqual(response.status_code, 400)
 
-        # 에러 메시지가 올바른지 확인
-        # expected_error = {"name": ["This field may not be blank."]}
-        # self.assertEqual(response.data, expected_error)
-
+    # 권한이 없는 사용자로 POST 요청 보내기
     def test_category_post_without_permission(self):
-        # 권한이 없는 사용자로 POST 요청 보내기
         self.client.login(username="testuser", password="testpass")
         data = {"name": "Category A"}
-        # response = self.client.post(self.URL, data, content_type="application/json")
-
-        # 응답 코드가 403인지 확인
-        response = self.client.get(f"{self.URL}{self.GROUP.pk}")
+        response = self.client.get(f"{self.URL}{self.group.pk}")
         self.assertEqual(response.status_code, 403)
 
 
 # 그룹 카테고리 수정 테스트
-class CategoriesPut(APITestCase):
+class GroupCategoriesPut(APITestCase):
     URL = "/api/v1/categories/"
     NAME = "Change test"
 
     def setUp(self):
-        self.GROUP = Group.objects.create(name="oz")
+        self.group = Group.objects.create(name="oz")
         self.upload_user = User.objects.create(username="Test User")
         self.category = Category.objects.create(
             name=self.NAME,
-            group=self.GROUP,
+            group=self.group,
         )
 
     # 존재하지 않는 url 접근
@@ -98,9 +101,10 @@ class CategoriesPut(APITestCase):
 
     # 비 로그인 유저가 수정
     def test_edit_category_detail_non_login_user(self):
-        data = {"name": "study"}
         response = self.client.put(
-            f"{self.URL}{self.GROUP.pk}{self.category.pk}", data, format="json"
+            f"{self.URL}{self.group.pk}/{self.category.pk}/",
+            data={"name": "Change test"},
+            format="json",
         )
         self.assertEqual(response.status_code, 403, "비 로그인 수정")
 
@@ -109,7 +113,7 @@ class CategoriesPut(APITestCase):
         user = User.objects.create(name="OtherUser", email="user@example.com")
         self.client.force_login(user)
         response = self.client.put(
-            f"{self.URL}{self.GROUP.pk}/{self.category.pk}/",
+            f"{self.URL}{self.group.pk}/{self.category.pk}/",
             data={"name": "Change test"},
             format="json",
         )
@@ -120,7 +124,7 @@ class CategoriesPut(APITestCase):
     # def test_edit_category_detail_create_user(self):
     #     self.client.force_login(self.upload_user)
     #     response = self.client.put(
-    #         f"{self.URL}{self.GROUP.pk}/{self.category.pk}/",
+    #         f"{self.URL}{self.group.pk}/{self.category.pk}/",
     #         data={"name": "Change test"},
     #         format="json",
     #     )
@@ -131,7 +135,7 @@ class CategoriesPut(APITestCase):
     def test_edit_task_detail_create_user_change_value(self):
         self.client.force_login(self.upload_user)
         response = self.client.put(
-            f"{self.URL}{self.GROUP.pk}/{self.category.pk}/",
+            f"{self.URL}{self.group.pk}/{self.category.pk}/",
             data={"name": "Change test"},
             format="json",
         )
